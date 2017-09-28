bf7c6b4efaec69ae0a1e702cae6ad20b5f4135dc
DeltaDizzy <EJBraswell@gmail.com>
2017-09-28 14:48:41 -0500

    Add files via upload

---
+++ AppLauncherController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using KSP.UI.Screens;
+
+namespace OptionalAtmopsheres2
+{
+    namespace UI
+    {
+        class AppLauncherController : MonoBehaviour
+        {
+            AtmosphereController OAToggler = new AtmosphereController();
+            public void ApplauncherOA()
+            {
+                bool OAButton = true;
+                if (KSP.UI.Screens.ApplicationLauncher.Ready && OAButton == true)
+                {
+                   
+
+                    
+                    Texture2D OATex = GameDatabase.Instance.GetTexture("SamBelanger/OptionalAtmospheres/OAButton.png", false);
+                    KSP.UI.Screens.ApplicationLauncher.Instance.AddModApplication(
+                            onTrue: AtmosphereController.AtmosphereToggler,
+                            onFalse: null,
+                            onHover: null,
+                            onHoverOut: null,
+                            onEnable: null,
+                            onDisable: null,
+                            visibleInScenes: KSP.UI.Screens.ApplicationLauncher.AppScenes.
+                                                  ALWAYS,
+                            texture: OATex);
+                }
+            }
+            
+        }
+    }
+}
